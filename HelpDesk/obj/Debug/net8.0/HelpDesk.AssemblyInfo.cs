70f0f764534fa9e3249e32a149531e48bdfd7d0d
LaurDumitrescu <dumitrescu.laur15@gmail.com>
2024-08-05 16:03:27 +0300

    running code

--- HelpDesk/obj/Debug/net8.0/HelpDesk.AssemblyInfo.cs
+++ HelpDesk/obj/Debug/net8.0/HelpDesk.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a23844bc315a40fe348e7e24b2d36f70f87451d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03145c8a2e844ed5f5c0cec20393d41fd7db7322")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
